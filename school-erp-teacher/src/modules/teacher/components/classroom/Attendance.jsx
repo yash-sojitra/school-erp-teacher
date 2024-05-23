0dbc9cc326315b32b75934ac006d25b13ff48d3c
yashsojitra97 <yashsojitra97@gmail.com>
2024-05-23 12:01:06 +0530

    added attendance

--- school-erp-teacher/src/modules/teacher/components/classroom/Attendance.jsx
+++ school-erp-teacher/src/modules/teacher/components/classroom/Attendance.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Checkbox } from "@/components/ui/checkbox";
+import { getTodaysDate, getTodaysDay } from "../../utils/dateFormatter";
+import { students } from "@/assets/data.json";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 const Attendance = () => {
-  return (
-    <div>
-      hellpo
-    </div>
-  )
-}
+  const [checkedItems, setCheckedItems] = useState([]);
 
-export default Attendance
+  const handleCheckboxChange = (event) => {
+    const { id, checked } = event.target;
+    if(checked){
+      checkedItems.push(id);
+    }
+    else{
+      checkedItems.pop(id);
+    }
+    setCheckedItems(checkedItems);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Checked items:", checkedItems);
+    // Process the checkedItems as needed
+  };
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold pt-4">Student Attendance</h1>
+        <p className="font-medium py-3">
+          {getTodaysDate() + ", " + getTodaysDay()}
+        </p>
+
+      {/* <form onSubmit={handleSubmit}>
+        {students.map((student) => {
+          return <Student key={student.id} student={student} />;
+        })}
+      </form> */}
+      <form onSubmit={handleSubmit}>
+        {students.map((student) => (
+          <div key={student.id} className="flex gap-6 items-center w-full bg-white rounded-xl my-4 p-4 shadow-md">
+            <input
+              type="checkbox"
+              name={student.name}
+              id={student.id}
+              checked={!!checkedItems[student.name]}
+              onChange={handleCheckboxChange}
+            />
+            <Avatar className="size-12">
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <h1 className="text-xl font-medium">{student.name}</h1>
+          </div>
+        ))}
+        <Button type="submit">Submit</Button>
+      </form>
+    </>
+  );
+};
+
+// const Student = ({ student }) => {
+//   return (
+
+//   );
+// };
+
+export default Attendance;
--- school-erp-teacher/src/modules/teacher/pages/ClassRoom.jsx
+++ school-erp-teacher/src/modules/teacher/pages/ClassRoom.jsx
@@ -3,17 +3,24 @@ import Attendance from "../components/classroom/Attendance";
 
 const ClassRoom = () => {
   return (
-    <Tabs defaultValue="account" className="w-full flex flex-col ml-6 border border-red-800">
-      <TabsList className="bg-primary">
-        <TabsTrigger className="text-2xl" value="attendance">Manage Attendance</TabsTrigger>
-        {/* <TabsTrigger className="text-2xl" value="assignment">Assignemt</TabsTrigger> */}
-        <TabsTrigger className="text-2xl" value="profile">Student Profile</TabsTrigger>
-      </TabsList>
-      <TabsContent value="attendance">
-        <Attendance/>
-      </TabsContent>
-      <TabsContent value="profile">Change your password here.</TabsContent>
-    </Tabs>
+      <Tabs
+        defaultValue="account"
+        className="w-full flex flex-col mx-6 md:mr-0"
+      >
+        <TabsList className="bg-primary justify-evenly p-6">
+          <TabsTrigger className="text-2xl" value="attendance">
+            Manage Attendance
+          </TabsTrigger>
+          {/* <TabsTrigger className="text-2xl" value="assignment">Assignemt</TabsTrigger> */}
+          <TabsTrigger className="text-2xl" value="profile">
+            Student Profile
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="attendance">
+          <Attendance />
+        </TabsContent>
+        <TabsContent value="profile">Change your password here.</TabsContent>
+      </Tabs>
   );
 };
 
